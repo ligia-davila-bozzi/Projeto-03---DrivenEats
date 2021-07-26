e93a8c8b3bf5b38a30df4e8fd1df0c618dee71b0
Douglas <douglas.07.nascimento@gmail.com>
2021-07-26 16:53:07 -0300

    qual o problema?

--- scripts.js
+++ scripts.js
@@ -1,34 +1,3 @@
-const comidinha = document.querySelector(".comidas");
-const bebidinha = document.querySelector(".bebidas");
-const sobremesinha = document.querySelector(".sobremesas");
-let valorComida = document.querySelector(".comidas > .preco > strong").innerHTML;
-let valorBebida = document.querySelector(".bebidas > .preco > strong").innerHTML;
-let valorSobremesa = document.querySelector(".sobremesas > .preco > strong").innerHTML;
-const ComidaSelect = document.querySelector(".comidas > .produto").innerHTML;
-const BebidaSelect = document.querySelector(".bebidas > .produto").innerHTML;
-const SobremesaSelect = document.querySelector(".sobremesas > .produto").innerHTML;
-
-valorComida = valorComida.replace(",",".");
-valorBebida = valorBebida.replace(",",".");
-valorSobremesa = valorSobremesa.replace(",",".");
-
-const Total = (valorComida + valorBebida + valorSobremesa);
-
-// function tiraVirgula (string){
-//     let valor;
-//     for (let i=0; i<string.length){
-//         if (string[i] !== ","){
-//             valor [i] = string[i];
-//         } else {
-//             valor [i] = ".";
-//         }
-//     }
-//     return valor;
-// }
-
-
-
-
 
 function ComidaSelecionada(ComidaClick) {
     const comidinha = document.querySelector(".comidas");
@@ -85,7 +54,19 @@ function AtivaBotao() {
      }
   }
 
-function FinalizaPedido (finaliza){
+function FinalizaPedido (){
+    let valorComida = document.querySelector(".comidas > .preco > strong").innerHTML;
+    let valorBebida = document.querySelector(".bebidas > .preco > strong").innerHTML;
+    let valorSobremesa = document.querySelector(".sobremesas > .preco > strong").innerHTML;
+    const ComidaSelect = document.querySelector(".comidas > .produto").innerHTML;
+    const BebidaSelect = document.querySelector(".bebidas > .produto").innerHTML;
+    const SobremesaSelect = document.querySelector(".sobremesas > .produto").innerHTML;
+
+    valorComida = valorComida.replace(",",".");
+    valorBebida = valorBebida.replace(",",".");
+    valorSobremesa = valorSobremesa.replace(",",".");
+
+    const Total = (valorComida + valorBebida + valorSobremesa);
     let MensagemWpp = encodeURIComponent(`Olá, gostaria de fazer o pedido:
     - Prato: ${ComidaSelect}
     - Bebida: ${BebidaSelect}
