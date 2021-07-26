fb353efa91c4fc71867ae50491e1b39c91b3d9a8
Douglas <douglas.07.nascimento@gmail.com>
2021-07-26 15:35:10 -0300

    Deus nos acuda

--- scripts.js
+++ scripts.js
@@ -1,6 +1,37 @@
+const comidinha = document.querySelector(".comidas");
+const bebidinha = document.querySelector(".bebidas");
+const sobremesinha = document.querySelector(".sobremesas");
+let valorComida = document.querySelector(".comidas > .preco > strong").innerHTML;
+let valorBebida = document.querySelector(".bebidas > .preco > strong").innerHTML;
+let valorSobremesa = document.querySelector(".sobremesas > .preco > strong").innerHTML;
+const ComidaSelect = document.querySelector(".comidas > .produto").innerHTML;
+const BebidaSelect = document.querySelector(".bebidas > .produto").innerHTML;
+const SobremesaSelect = document.querySelector(".sobremesas > .produto").innerHTML;
+
+valorComida = Number(tiraVirgula (valorComida));
+valorBebida = Number(tiraVirgula (valorBebida));
+valorSobremesa = Number(tiraVirgula (valorSobremesa));
+
+const Total = (valorComida + valorBebida + valorSobremesa);
+
+function tiraVirgula (string){
+    let valor;
+    for (let i=0; i<string.length){
+        if (string[i] !== ","){
+            valor [i] = string[i];
+        } else {
+            valor [i] = ".";
+        }
+    }
+    return valor;
+}
+
+
+
+
+
 function ComidaSelecionada(ComidaClick) {
-    const comidinha = document.querySelector(".comidas");
-    if (comidinha !== null) {
+        if (comidinha !== null) {
         comidinha.classList.toggle("comidas");
     }
 
@@ -10,7 +41,6 @@ function ComidaSelecionada(ComidaClick) {
 
 
 function BebidaSelecionada(BebidaClick) {
-    const bebidinha = document.querySelector(".bebidas");
     if (bebidinha !== null) {
         bebidinha.classList.toggle("bebidas");
     }
@@ -20,7 +50,6 @@ function BebidaSelecionada(BebidaClick) {
 }
 
 function SobremesaSelecionada(SobremesaClick) {
-    const sobremesinha = document.querySelector(".sobremesas");
     if (sobremesinha !== null) {
         sobremesinha.classList.toggle("sobremesas");
     }
@@ -30,29 +59,36 @@ function SobremesaSelecionada(SobremesaClick) {
 }
 
 function AtivaBotao() {
-    const comidinha = document.querySelector(".comidas");
-    const bebidinha = document.querySelector(".bebidas");
-    const sobremesinha = document.querySelector(".sobremesas");
-    const botao = document.querySelector(".Botao-Pedido");
-    const novobotao = document.querySelector(".desabilitado");
-    let i = 0;
 
+    const botao = document.querySelector(".Botao-Pedido");
+    let i = 0;
+  
     if (comidinha !== null) {
-        i += 1;
+      i += 1;
     }
     if (bebidinha !== null) {
-        i += 1;
+      i += 1;
     }
     if (sobremesinha !== null) {
-        i += 1;
+      i += 1;
     }
     if (i === 3) {
-        //troca botão de finalizar pedido
-        botao.classList.remove("Botao-Pedido");
-        botao.classList.add("desabilitado");
-        novobotao.classList.remove("desabilitado");
-        novobotao.classList.add("Fechar-Pedido");
-    }
+      //troca botão de finalizar pedido
+      botao.innerHTML = "Fechar Pedido";
+      botao.classList.remove("Botao-Pedido");
+      botao.classList.add("Fechar-Pedido");
+     }
+  }
+
+function FinalizaPedido (){
+    let MensagemWpp = encodeURIComponent(`Olá, gostaria de fazer o pedido:
+    - Prato: ${ComidaSelect}
+    - Bebida: ${BebidaSelect}
+    - Sobremesa: ${SobremesaSelect}
+    Total: ${Total}`);
+
+    window.open(`https://wa.me/5521994114784?text=${mensagemWpp}`, "_self");
+
 }
 
 
